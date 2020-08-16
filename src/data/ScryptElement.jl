19838f096727fa7bd41077975835b14441e10e14
Nicholas Bauer <nicholasbauer@outlook.com>
2020-08-16 16:26:48 -0400

    Removed unneede file, added compat and extras

--- src/data/ScryptElement.jl
+++
@@ -1,8 +0,0 @@
-struct ScryptElement
-    data::AbstractVector{UInt32}
-
-    function ScryptElement(bytes)
-        bytes % 64 == 0 || ArgumentError("Bytes must be a multiple of 64.") |> throw
-        data = zeros(UInt32, Int(bytes / sizeof(UInt32)))
-    end
-end
