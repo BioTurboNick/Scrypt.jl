2179c67b74cd654eea0ae31757f3bd432c11df37
Nicholas Bauer <nicholasbauer@outlook.com>
2025-12-24 00:04:09 -0500

    Patch `reinterpret` for Julia < 1.10

--- src/util.jl
+++ src/util.jl
@@ -42,3 +42,17 @@ function vprefetchnt(a::SIMD.FastContiguousArray{T,1}, i::Integer) where {T}
         prefetch(ptr, Val{0}(), Val{0}())
     end
 end
+
+if VERSION < v"1.10"
+    # Converting between bitstypes is new in 1.10; we'll only support just this specific conversion.
+    @inline function Base.reinterpret(::Type{NTuple{4, UInt8}}, x::UInt32)
+        in = Ref{UInt32}(x)
+        out = Ref{NTuple{4, UInt8}}()
+        GC.@preserve in out begin
+            ptr_in = unsafe_convert(Ptr{UInt32}, in)
+            ptr_out = unsafe_convert(Ptr{NTuple{4, UInt8}}, out)
+            memcpy(ptr_out, ptr_in, sizeof(NTuple{4, UInt8}))
+        end
+        return out[]
+    end
+end
