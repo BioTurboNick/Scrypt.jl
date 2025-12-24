d6f9134b42a5f94f83dfd24f3230f778e32b5409
Nicholas Bauer <nicholasbauer@outlook.com>
2025-12-23 23:09:32 -0500

    Remove LLVM code needed pre-1.6

--- src/util.jl
+++ src/util.jl
@@ -8,19 +8,7 @@ corresponds to prefetch0 on x86 (extreme locality)
 Adapted from SIMDPirates.jl
 """
 @generated function prefetch(ptr::Ptr{T}, ::Val{Locality}, ::Val{ReadOrWrite}) where {T, Locality, ReadOrWrite}
-    if VERSION < v"1.6.0-DEV.674"
-        prefetch_call_string = """%addr = inttoptr i$(8sizeof(Int)) %0 to i8*
-        call void @llvm.prefetch(i8* %addr, i32 $ReadOrWrite, i32 $Locality, i32 1)
-        ret void"""
-        quote
-            $(Expr(:meta, :inline))
-            Base.llvmcall(
-                ("declare void @llvm.prefetch(i8*, i32, i32, i32)",
-                $prefetch_call_string), Cvoid, Tuple{Ptr{$T}}, ptr
-            )
-        end
-    else
-        mod = """
+    mod = """
             declare void @llvm.prefetch(i8*, i32, i32, i32)
 
             define void @entry(i$(8sizeof(Int))) #0 {
@@ -32,12 +20,11 @@ Adapted from SIMDPirates.jl
 
             attributes #0 = { alwaysinline }
         """
-        quote
-            $(Expr(:meta, :inline))
-            Base.llvmcall(
-                ($mod, "entry"), Cvoid, Tuple{Ptr{$T}}, ptr
-            )
-        end
+    quote
+        $(Expr(:meta, :inline))
+        Base.llvmcall(
+            ($mod, "entry"), Cvoid, Tuple{Ptr{$T}}, ptr
+        )
     end
 end
 
@@ -52,28 +39,5 @@ function vprefetchnt(a::SIMD.FastContiguousArray{T,1}, i::Integer) where {T}
     GC.@preserve a begin
         ptr = @inbounds pointer(a, i)
         prefetch(ptr, Val{0}(), Val{0}())
-    end
-end
-
-
-if VERSION < v"1.6.0-DEV"
-    import Base.stride
-    import Base.strides
-
-    function stride(a::Base.ReinterpretArray, i::Int)
-        a.parent isa StridedArray || ArgumentError("Parent must be strided.") |> throw
-        if i > ndims(a)
-            return length(a)
-        end
-        s = 1
-        for n = 1:(i-1)
-            s *= size(a, n)
-        end
-        return s
-    end
-
-    function strides(a::Base.ReinterpretArray)
-        a.parent isa StridedArray || ArgumentError("Parent must be strided.") |> throw
-        Base.size_to_strides(1, size(a)...)
     end
 end
