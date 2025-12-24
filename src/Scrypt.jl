c1e8e7418af9141e0b657445e434c998e19d3b83
Nicholas Bauer <nicholasbauer@outlook.com>
2025-12-23 19:56:43 -0500

    Increase performance by eliminating allocations and avoiding `@simd` macro expansion in hot loop (#5)
    Co-authored-by: Dr. Jiacheng Chuan <jiacheng_chuan@outlook.com>

--- src/Scrypt.jl
+++ src/Scrypt.jl
@@ -1,6 +1,7 @@
 module Scrypt
 
-using Nettle
+import Nettle: HMACState, digest!, update!
+import Nettle_jll: libnettle
 using SIMD
 
 include("data/Salsa512.jl")
@@ -13,69 +14,84 @@ function scrypt(parameters::ScryptParameters, key::Vector{UInt8}, salt::Vector{U
     buffer = pbkdf2_sha256_1(key, salt, bufferlength(parameters))
     parallelbuffer = reshape(reinterpret(Salsa512, buffer), (elementblockcount(parameters), parameters.p))
 
+    scryptblockbuffer = alloc_scryptblock_buffer(parameters)
+    workingbuffer = alloc_element_buffer(parameters)
+    shufflebuffer = alloc_element_buffer(parameters)
+
     for i ∈ 1:parameters.p
         element = @views reshape(parallelbuffer[:, i], elementblockcount(parameters))
-        smix!(element, parameters)
+        smix!(element, parameters, scryptblockbuffer, workingbuffer, shufflebuffer)
     end
 
     derivedkey = pbkdf2_sha256_1(key, buffer, derivedkeylength)
+end
+
+function alloc_scryptblock_buffer(parameters::ScryptParameters)
+    r, N = parameters.r, parameters.N
+    reshape(valloc(Salsa512, 2r * N), (2r, N))
+end
+
+function alloc_element_buffer(parameters::ScryptParameters)
+    valloc(Salsa512, 2parameters.r)
 end
 
 const HASH_LENGTH = 256 ÷ 8
 
 function pbkdf2_sha256_1(key, salt::Vector{UInt8}, derivedkeylength)
     blockcount = cld(derivedkeylength, HASH_LENGTH)
-    lastblockbytes = derivedkeylength - (blockcount - 1) * HASH_LENGTH
     
-    salt = [salt; zeros(UInt8, 4)]
+    push!(salt, 0x00, 0x00, 0x00, 0x00)
     salttail = view(salt, length(salt) - 3:length(salt))
     
     derivedkey = Matrix{UInt8}(undef, HASH_LENGTH, blockcount)
 
+    state = HMACState("SHA256", key)
+    digestbuffer = Vector{UInt8}(undef, state.hash_type.digest_size)
     for i ∈ 1:blockcount
-        salttail[:] = reinterpret(UInt8, [UInt32(i)]) |> reverse
-        derivedkey[:, i] = digest("sha256", key, salt)
+        salttail[:] .= reinterpret(NTuple{4, UInt8}, UInt32(i)) |> reverse
+        derivedkey[:, i] = digest!(digestbuffer, update!(state, salt))
     end
 
-    derivedkey = reshape(derivedkey, blockcount * HASH_LENGTH)[1:derivedkeylength]
+    derivedkey = resize!(vec(derivedkey), derivedkeylength)
     return derivedkey
 end
 
-function smix!(element::AbstractVector{Salsa512}, parameters::ScryptParameters)
-    workingbuffer = prepare(element)
-    shufflebuffer = valloc(Salsa512, length(workingbuffer))
-    scryptblock, workingbuffer, shufflebuffer = fillscryptblock!(workingbuffer, shufflebuffer, parameters.r, parameters.N)
+function digest!(digestbuffer::Vector{UInt8}, state::HMACState)
+    length(digestbuffer) == state.hash_type.digest_size ||
+        throw(DimensionMismatch("`digestbuffer` must be of length $(state.hash_type.digest_size)"))
+    ccall((:nettle_hmac_digest, libnettle), Cvoid, (Ptr{Cvoid}, Ptr{Cvoid}, Ptr{Cvoid}, Ptr{Cvoid}, Csize_t,
+        Ptr{UInt8}), state.outer, state.inner, state.state, state.hash_type.ptr, sizeof(digestbuffer), digestbuffer)
+    return digestbuffer
+end
+
+function smix!(element::AbstractVector{Salsa512}, parameters::ScryptParameters, scryptblockbuffer::AbstractMatrix{Salsa512}, workingbuffer::AbstractVector{Salsa512}, shufflebuffer::AbstractVector{Salsa512})
+    prepare!(workingbuffer, element)
+    scryptblock, workingbuffer, shufflebuffer = fillscryptblock!(scryptblockbuffer, workingbuffer, shufflebuffer, parameters.r, parameters.N)
     workingbuffer = mixwithscryptblock!(workingbuffer, scryptblock, shufflebuffer, parameters.r, parameters.N)
     restore!(element, workingbuffer)
 end
 
 const SALSA_BLOCK_REORDER_INDEXES = [13;  2;  7; 12;  1;  6; 11; 16;  5; 10; 15;  4;  9; 14;  3;  8]
 
-function prepare(src::AbstractVector{Salsa512})
-    dest = valloc(Salsa512, length(src))
-    si = 1:length(src)
-    dj = [2:length(dest); 1]
-
-    for (i, j) ∈ zip(si, dj)
-        dest[j] = src[i]
-        permute!(uint32view(dest, j), SALSA_BLOCK_REORDER_INDEXES)
+function prepare!(dest::AbstractVector{Salsa512}, src::AbstractVector{Salsa512})
+    n = length(src)
+    uint32view(dest, 1) .= @view uint32view(src, n)[SALSA_BLOCK_REORDER_INDEXES]
+    for i ∈ 2:n
+        uint32view(dest, i) .= @view uint32view(src, i - 1)[SALSA_BLOCK_REORDER_INDEXES]
     end
-
     return dest
-end #permute! is no faster than explicit vectorization, even with a few extra allocations
+end
 
 function restore!(dest::AbstractVector{Salsa512}, src::AbstractVector{Salsa512})
-    si = 1:length(src)
-    dj = [length(dest); 1:length(dest) - 1]
-
-    for (i, j) ∈ zip(si, dj)
-        dest[j] = src[i]
-        invpermute!(uint32view(dest, j), SALSA_BLOCK_REORDER_INDEXES)
+    length(dest) == length(src) || throw(DimensionMismatch("dest and src must have the same length"))
+    n = length(src)
+    uint32view(dest, n)[SALSA_BLOCK_REORDER_INDEXES] .= uint32view(src, 1)
+    for i ∈ 2:n
+        uint32view(dest, i - 1)[SALSA_BLOCK_REORDER_INDEXES] .= uint32view(src, i)
     end
 end
 
-function fillscryptblock!(workingbuffer::AbstractVector{Salsa512}, shufflebuffer::AbstractVector{Salsa512}, r, N)
-    scryptblock = reshape(valloc(Salsa512, 2r * N), (2r, N))
+function fillscryptblock!(scryptblock::AbstractMatrix{Salsa512}, workingbuffer::AbstractVector{Salsa512}, shufflebuffer::AbstractVector{Salsa512}, r, N)
     for i ∈ 1:N
         scryptelement = view(scryptblock, :, i)
         previousblock = lastblock = load_store!(workingbuffer, scryptelement, 1)
@@ -104,7 +120,7 @@ function load_store!(workingbuffer::AbstractVector{Salsa512}, scryptelement::Abs
     return block
 end
 
-function mixwithscryptblock!(workingbuffer::AbstractVector{Salsa512}, scryptblock, shufflebuffer::AbstractVector{Salsa512}, r, N)
+function mixwithscryptblock!(workingbuffer::AbstractVector{Salsa512}, scryptblock::AbstractMatrix{Salsa512}, shufflebuffer::AbstractVector{Salsa512}, r, N)
     for i ∈ 1:N
         n = integerify(workingbuffer, N)
         scryptelement = reshape(view(scryptblock, :, n), 2r)
@@ -177,10 +193,11 @@ function salsamix(lines)
 end
 
 function salsa(addend1, addend2, xor_operand, rotationmagnitude)
-    sum = addend1 + addend2
-    rot = (sum << rotationmagnitude) | (sum >>> (sizeof(UInt32) * 8 - rotationmagnitude))
-    return xor_operand ⊻ rot
+    sum = Tuple(addend1) .+ Tuple(addend2)
+    rot = (sum .<< rotationmagnitude) .| (sum .>>> (sizeof(UInt32) * 8 - rotationmagnitude))
+    return Vec{4, UInt32}(Tuple(xor_operand) .⊻ rot)
 end
+# @simd macro expansion is heavy, converting to tuples and broadcasting operations is faster
 
 function salsatranspose(lines)
     toline3 = @inbounds shufflevector(lines[1], Val((1, 2, 3, 0)))
